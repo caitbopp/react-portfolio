4c14961aed180ba546b4d36d99f7c47b49910f9a
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-22 18:42:16 -0400

    Updated folder structure for pages and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
-import AboutMe from './components/AboutMe';
-import Portfolio from './components/Portfolio';
+import AboutMe from './components/pages/AboutMe';
+import Portfolio from './components/pages/Portfolio';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import { HashRouter as Router, Route } from 'react-router-dom';
