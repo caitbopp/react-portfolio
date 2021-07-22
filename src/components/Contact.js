689dda8e5ac322312e64e5a2302a5edac8d1da4f
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-22 18:39:15 -0400

    Updated contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,25 +7,14 @@ export default function Contact() {
 
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
                 <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                    <iframe
-                        width="100%"
-                        height="100%"
-                        title="map"
-                        className="absolute inset-0"
-                        frameBorder={0}
-                        marginHeight={0}
-                        marginWidth={0}
-                        style={{ filter: "opacity(0.7)" }}
-                        src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-                    />
                     <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
                         <div className="lg:w-1/2 px-6">
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 ADDRESS
                             </h2>
                             <p className="mt-1">
-                                97 Warren St. <br />
-                                New York, NY 10007
+                                2206 Lake Ave. <br />
+                                Baltimore, MD 21213
                             </p>
                         </div>
                         <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -33,12 +22,12 @@ export default function Contact() {
                                 EMAIL
                             </h2>
                             <a className="text-indigo-400 leading-relaxed">
-                                reedbarger@email.com
+                                boppcaitlin@gmail.com
                             </a>
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                                 PHONE
                             </h2>
-                            <p className="leading-relaxed">123-456-7890</p>
+                            <p className="leading-relaxed">410-868-2677</p>
                         </div>
                     </div>
                 </div>
