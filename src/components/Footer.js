ffd8b09804e5ab614ba96853f91382879453eccc
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-22 19:03:42 -0400

    Simplified footer and updated styling to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,29 +2,10 @@ import React from 'react';
 
 export default function Footer() {
     return (
-<div class="flex-row container" id="contact-me">
-
-        <div class="contact-information">
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#">410.868.2677</a>
-                    </li>
-                    <li>
-                        <a href="mailto:boppcaitlin@gmail.com">boppcaitlin@gmail.com</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/caitbopp">GitHub</a>
-                    </li>
-                    <li>
-                        <a href="https://linkedin.com/in/caitlinbopp">LinkedIn</a>
-                    </li>
-                </ul>
-            </nav>
-
+        <div className="footer text-center">
+            <p><a href="#">410.868.2677</a> | <a href="mailto:boppcaitlin@gmail.com">boppcaitlin@gmail.com</a> | <a href="https://github.com/caitbopp">GitHub</a> | <a href="https://linkedin.com/in/caitlinbopp">LinkedIn</a>
+            </p>
         </div>
 
-
-    </div>
     );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -29,7 +29,7 @@ export default function Projects() {
               </div>
 
         ))}
-                  </section>;
+                  </section>
 
       </div>
     // </section>
