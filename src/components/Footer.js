964fe534d6bc479e775e2d20c1b772008ee2ace6
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-22 18:39:50 -0400

    Created Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+<div class="flex-row container" id="contact-me">
+
+        <div class="contact-information">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">410.868.2677</a>
+                    </li>
+                    <li>
+                        <a href="mailto:boppcaitlin@gmail.com">boppcaitlin@gmail.com</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/caitbopp">GitHub</a>
+                    </li>
+                    <li>
+                        <a href="https://linkedin.com/in/caitlinbopp">LinkedIn</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
+
+
+    </div>
+    );
+}
