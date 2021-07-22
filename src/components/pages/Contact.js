b5032f7b72c13984d2b313f3bdc6f4b298dfa780
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-22 19:22:58 -0400

    Updated fized position for footer

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,27 +8,27 @@ export default function Contact() {
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
                 <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
                     <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-                        <div className="lg:w-1/2 px-6">
+
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 ADDRESS
                             </h2>
-                            <p className="mt-1">
+                            <p>
                                 2206 Lake Ave. <br />
                                 Baltimore, MD 21213
                             </p>
-                        </div>
-                        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 EMAIL
                             </h2>
-                            <a className="text-indigo-400 leading-relaxed">
+                            <p><a href="mailto:boppcaitlin@gmail.com">
                                 boppcaitlin@gmail.com
                             </a>
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                            </p>
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 PHONE
                             </h2>
                             <p className="leading-relaxed">410-868-2677</p>
-                        </div>
+
                     </div>
                 </div>
                 <form
@@ -36,12 +36,8 @@ export default function Contact() {
                     name="contact"
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                        Hire Me
+                       Let's Connect!
                     </h2>
-                    <p className="leading-relaxed mb-5">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-                        suscipit officia aspernatur veritatis. Asperiores, aliquid?
-                    </p>
                     <div className="relative mb-4">
                         <label htmlFor="name" className="leading-7 text-sm text-gray-400">
                             Name
