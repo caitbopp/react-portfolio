314216bda6d7be0d5c42b1766686b0604c6c9874
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-23 12:39:25 -0400

    Updated styling on Contact page

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -8,13 +8,11 @@ export default function AboutMe() {
             <div className="about-me-container mb-5">
                 <div className="row">
                     <div className="span4">
-                        <div className="clearfix content-heading">
-                            <img className="pull-left" id="headshot" src={headshot} />
-                            <p>Hello! My name's Caitlin and I'm an aspiring web developer. I've worked in Marketing for the last 7 years,
-                                and while I've learned a lot and had fun, I'm looking to change my career to developing. I'm very
-                                detailed-oriented, organized, and I love problem-solving! If you like any of the work you see below, please
-                                reach out - I'd love the opportunity to connect!</p>
-                        </div>
+                        <img className="pull-left" id="headshot" src={headshot} />
+                        <p>Hello! My name's Caitlin and I'm an aspiring web developer. I've worked in Marketing for the last 7 years,
+                            and while I've learned a lot and had fun, I'm looking to change my career to developing. I'm very
+                            detailed-oriented, organized, and I love problem-solving! If you like any of the work you see below, please
+                            reach out - I'd love the opportunity to connect!</p>
                     </div>
                 </div>
             </div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,77 +5,81 @@ export default function Contact() {
         <div>
             <h1>Let's Connect!</h1>
 
-            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-                <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="contact-me-container row">
 
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                                ADDRESS
-                            </h2>
-                            <p>
-                                2206 Lake Ave. <br />
-                                Baltimore, MD 21213
-                            </p>
 
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                                EMAIL
-                            </h2>
-                            <p><a href="mailto:boppcaitlin@gmail.com">
-                                boppcaitlin@gmail.com
-                            </a>
-                            </p>
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                                PHONE
-                            </h2>
-                            <p className="leading-relaxed">410-868-2677</p>
+                <div className="address-card col-sm-6">
+                    <div className="card-body">
+                        <h2 className="title-font font-semibold text-white tracking-widest text-xs" className="title-font font-semibold text-white tracking-widest text-xs">
+                            ADDRESS
+                        </h2>
+                        <p>
+                            2206 Lake Ave. <br />
+                            Baltimore, MD 21213
+                        </p>
+
+                        <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                            EMAIL
+                        </h2>
+                        <p><a href="mailto:boppcaitlin@gmail.com">
+                            boppcaitlin@gmail.com
+                        </a>
+                        </p>
+                        <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                            PHONE
+                        </h2>
+                        <p>410-868-2677</p>
+                    </div>
+                </div>
+
+                <div className="contact-card col-sm-6">
+                    <div className="card-body">
+                        <form className="connect-form">
+                            <div className="relative mb-4">
+                                <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                                    Name
+                                </label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                />
+                            </div>
+                            <div className="relative mb-4">
+                                <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                                    Email
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                />
+                            </div>
+                            <div className="relative mb-4">
+                                <label
+                                    htmlFor="message"
+                                    className="leading-7 text-sm text-gray-400">
+                                    Message
+                                </label>
+                                <textarea
+                                    id="message"
+                                    name="message"
+                                    className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="border-0 py-2 px-6 focus:outline-none rounded text-lg">
+                                Submit
+                            </button>
+                        </form>
 
                     </div>
                 </div>
-                <form
-                    netlify
-                    name="contact"
-                    className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-                    <div className="relative mb-4">
-                        <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-                            Name
-                        </label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                        />
-                    </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                        />
-                    </div>
-                    <div className="relative mb-4">
-                        <label
-                            htmlFor="message"
-                            className="leading-7 text-sm text-gray-400">
-                            Message
-                        </label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                        Submit
-                    </button>
-                </form>
             </div>
         </div>
+
     );
 };
