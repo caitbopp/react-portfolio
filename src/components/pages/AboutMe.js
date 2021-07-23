4591bc7626f31ca0240de7985d6782806e2f31aa
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-23 13:13:36 -0400

    Updated README

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,7 +11,7 @@ export default function AboutMe() {
                         <img className="pull-left" id="headshot" src={headshot} />
                         <p>Hello! My name's Caitlin and I'm an aspiring web developer. I've worked in Marketing for the last 7 years,
                             and while I've learned a lot and had fun, I'm looking to change my career to developing. I'm very
-                            detailed-oriented, organized, and I love problem-solving! If you like any of the work you see below, please
+                            detailed-oriented, organized, and I love problem-solving! If you like any of the work you see in my porfolio, please
                             reach out - I'd love the opportunity to connect!</p>
                     </div>
                 </div>
