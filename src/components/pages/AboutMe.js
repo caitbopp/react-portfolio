c862ab99f33fe1a3a2548d808d379c8f90b83e34
Caitlin Bopp <boppcaitlin@gmail.com>
2021-09-10 16:54:11 -0400

    Updated About Me section

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,7 +11,7 @@ export default function AboutMe() {
                         <img className="pull-left" id="headshot" src={headshot} />
                         <p>Hello! My name's Caitlin and I'm a web developer from Baltimore, Maryland. I recently completed a full stack web development bootcamp and am currently pursuing a career as a front-end developer. I'm very
                             detailed-oriented, organized, and I love problem-solving! If you like any of the work you see in my porfolio, please
-                            reach out - I'd love the opportunity to connect!</p>
+                            reach out, I'd love the opportunity to connect!</p>
                     </div>
                 </div>
             </div>
